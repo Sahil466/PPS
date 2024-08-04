070b73e3f56412d7ce8ef648adfc90a42f4929d7
Sahil Sojitra <sahilsojitra273@gamil.com>
2024-08-04 11:32:58 +0530

    pre-28

--- pre-28.c
+++ pre-28.c
@@ -1,4 +1,11 @@
-#include<stdion.h>
+#include<stdio.h>
 int main(){
+    int a,b,sum;
+    printf("Enter the number : ");
+    scanf("%d",&a);
+    printf("Enter the number : ");
+    scanf("%d", &b);
+    sum=a+b;
+    printf(" the sum is : %d",sum);
     return 0;
 }
